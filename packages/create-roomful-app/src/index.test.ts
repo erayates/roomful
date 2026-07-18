a3ac78b15e49a496a8e155133e534f128d2010c0
erayates <erayates@users.noreply.github.com>
2026-07-18 13:48:35 +0300

    fix: sort imports in test file

--- packages/create-roomful-app/src/index.test.ts
+++ packages/create-roomful-app/src/index.test.ts
@@ -1,4 +1,4 @@
-import { describe, it, expect } from 'vitest';
+import { describe, expect, it } from 'vitest';
 
 describe('create-roomful-app', () => {
   it('should have a proper package.json', async () => {
